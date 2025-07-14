39ddc9e67bbf3d52a7a1e394921d5d01305468ca
hyomin-dev <choihyomin248@gmail.com>
2025-07-15 00:50:36 +0900

    [Bronze III] Title: ACM 호텔, Time: 132 ms, Memory: 16204 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t>0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int yy,xx;
+            if(n%h==0) {
+                yy = h;
+                xx = n/h;
+            }
+            else {
+                yy = n % h;
+                xx = n/h+1;
+            }
+
+
+            if(xx<10){
+                bw.write(yy+"0"+xx);
+            }else{
+                bw.write(String.valueOf(yy)+String.valueOf(xx));
+            }
+            bw.newLine();
+
+            t--;
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
