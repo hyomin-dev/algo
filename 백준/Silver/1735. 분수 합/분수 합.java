49b6d92c18c692142e8efb037435bce3f9d795bf
hyomin-dev <choihyomin248@gmail.com>
2025-07-18 23:40:00 +0900

    [Silver III] Title: 분수 합, Time: 1020 ms, Memory: 19696 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n1Up = Integer.parseInt(st.nextToken());
+        int n1Down = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int n2Up = Integer.parseInt(st.nextToken());
+        int n2Down = Integer.parseInt(st.nextToken());
+
+        int nUp = n1Up*n2Down + n2Up*n1Down;
+        int nDown = n1Down*n2Down;
+
+        int end = Math.min(nUp,nDown);
+        for(int i=end;i>=2;i--){
+            if(nUp%i==0&&nDown%i==0){
+                nUp = nUp/i;
+                nDown = nDown/i;
+                break;
+            }
+        }
+
+        bw.write(nUp+" "+nDown);
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
