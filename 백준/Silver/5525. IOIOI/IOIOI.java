c8e3d2077e11a6cc4e6be6b9e0cdf47a9b25a3d8
hyomin-dev <choihyomin248@gmail.com>
2025-07-21 01:25:16 +0900

    [Silver I] Title: IOIOI, Time: 2336 ms, Memory: 20232 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -2,29 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n,m, findStrLength;
+    static int count = 0;
+    static String s;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        String s = br.readLine();
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        s = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
+        findStrLength = 2*n+1;
+        count = 0;
 
-        for(int i=0;i<2*n+1;i++){
-            if(i%2==0)
-                sb.append("I");
-            else
-                sb.append("O");
-        }
+        int i=0;
+        while(i<=m-findStrLength){
+            if(s.charAt(i)=='O'){
+                i++;
+            }else{
+                boolean result = findStr(i);
+                if(result) {
+                    i += 2;
+                    count++;
+                }else{
+                    i++;
+                }
+                while(result){
+                    if(i+2*(n-1)+2>=m)
+                        break;
 
-        String findStr = sb.toString();
-        int findStrLength = findStr.length();
-        int count = 0;
-        for(int i=0;i<=m-findStrLength;i++){
-            if(findStr.equals(s.substring(i,i+findStrLength)))
-                count++;
+                    if(s.charAt(i+2*(n-1)+1)=='O'&&s.charAt(i+2*(n-1)+2)=='I') {
+                        i+=2;
+                        count++;
+                        result = true;
+                    }
+                    else{
+                        i = i+2*(n-1);
+                        result = false;
+                    }
+
+                }
+
+            }
         }
 
         bw.write(count+"");
@@ -32,5 +52,28 @@ public class Main {
         bw.flush();
         bw.close();
         br.close();
+    }
+    static boolean findStr(int startIdx){
+
+        int oiCount = 0;
+        int idx = startIdx;
+        while(true){            
+
+            if(oiCount==n) {
+                break;
+            }
+
+            if(idx+2>=m)
+                return false;
+
+            if(s.charAt(idx+1)=='O'&&s.charAt(idx+2)=='I') {
+                oiCount++;
+                idx += 2;
+            } else
+                return false;
+        }
+
+        return true;
+
     }
 }
