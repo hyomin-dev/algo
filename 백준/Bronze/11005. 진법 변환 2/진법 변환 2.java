83df316a8c79e4642927aa1b1651c92345590ccc
hyomin-dev <choihyomin248@gmail.com>
2025-07-23 01:03:43 +0900

    [Bronze I] Title: 진법 변환 2, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(n>=b){
+
+            int r = n%b;
+            n = n/b;
+
+            if(r<10){
+                sb.append(String.valueOf(r));
+            }else{
+                sb.append((char)(r+55));
+            }
+
+        }
+        if(n<10){
+            sb.append(String.valueOf(n));
+        }else{
+            sb.append((char)(n+55));
+        }
+
+        String nStr = sb.toString();
+        for(int i=nStr.length()-1;i>=0;i--)
+            bw.write(nStr.charAt(i));
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
+
+/*
+
+각 자리의 숫자마다 곱해야하는 수
+1
+1*b
+1*b*b
+
+0~9인 경우: 그대로 숫자 사용
+
+A: 65
+Z: 90
+A~Z인 경우: 변환
+
+문자 - '65' + 10
+
+
+* */
