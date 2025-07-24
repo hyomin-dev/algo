04097d41b1b92ec11122c1211251a113d1b6627a
hyomin-dev <choihyomin248@gmail.com>
2025-07-24 19:32:56 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 968 ms, Memory: 97140 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n,m;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int[] visitedList;
+    static int count = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n+1];
+        visitedList = new int[n+1];
+
+        for(int i=0;i<=n;i++)
+            graph.add(new ArrayList<>());
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for(int i=1;i<=n;i++)
+            Collections.sort(graph.get(i),Collections.reverseOrder());
+
+        visited[r] = true;
+        bfs(r);
+
+        for(int i=1;i<=n;i++)
+            bw.write(visitedList[i]+"\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void bfs(int node){
+        visitedList[node] = count++;
+
+        for(int i=0;i<graph.get(node).size();i++){
+            int adjcencyNode = graph.get(node).get(i);
+            if(!visited[adjcencyNode]){
+                visited[adjcencyNode] = true;
+                bfs(adjcencyNode);
+            }
+        }
+    }
+}
