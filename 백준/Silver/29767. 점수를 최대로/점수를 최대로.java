e57a08b4824981abeb240d378118baba11625d74
hyomin-dev <choihyomin248@gmail.com>
2025-07-25 22:22:27 +0900

    [Silver IV] Title: 점수를 최대로, Time: 740 ms, Memory: 57632 KB -BaekjoonHub

---
+++ 백준/Silver/29767. 점수를 최대로/점수를 최대로.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        long[] array = new long[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++)
+            array[i] = Integer.parseInt(st.nextToken());
+
+
+        for(int i=1;i<n;i++) {
+            array[i] = array[i] + array[i-1];
+        }
+
+        Arrays.sort(array);
+
+        long result = 0;
+        int idx = n-1;
+        while(k>0){
+            result +=array[idx--];
+            k--;
+        }
+
+        bw.write(result+"");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
