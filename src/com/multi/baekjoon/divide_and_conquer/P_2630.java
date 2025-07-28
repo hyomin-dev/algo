696ffffaa93c674934f1f6267f9767b55b066da2
hyomin-dev <choihyomin248@gmail.com>
2025-07-28 23:52:17 +0900

    7/28

--- src/com/multi/baekjoon/divide_and_conquer/P_2630.java
+++ src/com/multi/baekjoon/divide_and_conquer/P_2630.java
@@ -5,57 +5,52 @@ import java.util.*;
 
 
 public class P_2630 {
-    static int n;
     static int[][] paper;
-    static boolean[][] visited;
+    static int white = 0, blue = 0; // white: 0, blue: 1
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
-        paper = new int[n+1][n+1];
-        visited = new boolean[n+1][n+1];
+        int n = Integer.parseInt(br.readLine());
+        paper = new int[n][n];
 
-        for(int y=1;y<=n;y++){
+        for(int y=0;y<n;y++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int x=1;x<=n;x++){
+            for(int x=0;x<n;x++){
                 paper[y][x] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int s=16;s>=1;s/=2){
-            int start = 1;
-            int end = s;
-            // System.out.println(y+"~"+(y+s-1));
-            for(int y=start;y<=16;y+=s){
-                for(int x=start;x<=16;x+=s){
+        search(n,0,0);
 
+        bw.write(String.valueOf(white));
+        bw.newLine();
+        bw.write(String.valueOf(blue));
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void search(int n, int startX, int startY){
+        for(int y=startY;y<startY+n;y++){
+            for(int x=startX;x<startX+n;x++){
+                if(paper[y][x]!=paper[startY][startX]){
+                    int halfN = n/2;
+                    search(halfN,startX, startY);
+                    search(halfN,startX+halfN,startY);
+                    search(halfN,startX,startY+halfN);
+                    search(halfN,startX+halfN,startY+halfN);
+                    return;
                 }
-
             }
-
         }
 
-    }
+        if(paper[startY][startX]==0)
+            white++;
+        else
+            blue++;
 
-    static boolean search(int startY, int endY, int startX, int endX, int value){
-        return false;
     }
 }
-
-/*
-8
-0~1, 2~3, 4~5, 6~7
-
-1~2, 3~4, 5~6, 7~8
-
---> 1부터 시작하도록 함
-
-16이라면
-1. 1~16 --> 16/1
-2. 1~8, 9~16 --> 16/2
-3. 1~4, 5~8, 9~12, 13~16 --> 16/4
-4. 1~2, 3~4, 5~6, 7~8, 9~10, 11~12, 13~14, 15~16 --> 16/8
-5. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 --> 16/16
-
-* */
