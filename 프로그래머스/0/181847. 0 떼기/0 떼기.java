05af2ff872daf81837ef59e32e8e6ce32d5bfedf
hyomin-dev <choihyomin248@gmail.com>
2026-05-25 17:36:09 +0900

    [level 0] Title: 0 떼기, Time: 4.63 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String n_str) {
+         int idx = 0;
+
+       for(int i=0;i<=n_str.length();i++){
+            int num = Integer.parseInt(n_str.charAt(i)+"");
+            if(num!=0)
+                break;
+            else
+                idx++;
+       }
+
+       return n_str.substring(idx);
+    }
+}
