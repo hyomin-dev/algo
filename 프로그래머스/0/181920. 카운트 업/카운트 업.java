108460dfbff34b3c51132f07f852f4f3ae35ad00
hyomin-dev <choihyomin248@gmail.com>
2026-05-25 17:24:19 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 61.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] result = new int[end_num-start_num+1];
+        int idx = 0;
+
+        for(int i=start_num;i<=end_num;i++)
+            result[idx++] = i;
+
+        return result;
+    }
+}
