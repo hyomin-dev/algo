315e14c7e1436e77fcce132ca4502ef923c0f8da
hyomin-dev <choihyomin248@gmail.com>
2026-05-26 05:00:29 +0900

    [level 0] Title: 접미사 배열, Time: 0.33 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] arr = new String[my_string.length()];
+        
+        for(int i=my_string.length()-1;i>=0;i--){
+            arr[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(arr);
+        
+        return arr;
+    }
+}
