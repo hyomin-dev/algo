26882dfee43374f24a2d4a0f109571523963abf3
hyomin-dev <choihyomin248@gmail.com>
2026-05-28 06:28:31 +0900

    [level 0] Title: 리스트 자르기, Time: 1.74 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+             
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        switch(n){
+            case 1 ->{
+                for(int i=0;i<=slicer[1];i++)
+                    list.add(num_list[i]);
+            }
+            case 2 ->{
+                for(int i=slicer[0];i<num_list.length;i++)
+                    list.add(num_list[i]);
+            }
+            case 3 ->{
+                for(int i=slicer[0];i<=slicer[1];i++)
+                    list.add(num_list[i]);
+            }
+            case 4 ->{
+                for(int i=slicer[0];i<=slicer[1];i+=slicer[2])
+                    list.add(num_list[i]);
+            }
+                
+                
+        }
+        
+        return list.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
